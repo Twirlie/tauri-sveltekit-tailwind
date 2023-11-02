307c5f9cd1df86f3cf867bd0531d1e165c11937f
Twirlie <popadopz@gmail.com>
2023-11-01 22:42:37 -0500

    Add Tailwind

--- src/lib/Greet.svelte
+++ src/lib/Greet.svelte
@@ -9,8 +9,17 @@
   }
 </script>
 
-<div>
-  <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-  <button on:click={greet}>Greet</button>
-  <p>{greetMsg}</p>
+<div class="justify-center m-4 text-center grid place-items-center">
+  <input
+    class="bg-slate-800 h-9 p-1 m-1 border-2 border-slate-600 rounded-lg"
+    id="greet-input"
+    placeholder="Enter a name..."
+    bind:value={name}
+  />
+  <button class="bg-green-500 p-1 m-1 border-2 border-green-600 rounded-lg" on:click={greet}
+    >Greet</button
+  >
+  {#if greetMsg}
+    <p class="bg-blue-500 w-10/12 p-1 m-1 border-2 border-blue-600 rounded-lg">{greetMsg}</p>
+  {/if}
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,14 @@
   import Greet from '../lib/Greet.svelte'
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<Greet />
+<div class="justify-center text-center border-gray-950 m-4">
+  <h1 class="text-3xl font-bold p-4">Welcome to SvelteKit</h1>
+  <Greet />
+</div>
+
+<style lang="postcss">
+  :global(html) {
+    color: theme(colors.white);
+    background-color: theme(colors.gray.700);
+  }
+</style>
